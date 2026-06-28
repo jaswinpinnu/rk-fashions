44b69d173e9ff2d5278a629b3fc4a7a0471c855f
jaswinpinnu <162045165+jaswinpinnu@users.noreply.github.com>
2026-06-28 15:40:22 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+importScripts("https://www.gstatic.com/firebasejs/12.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCO9YJ1SoZbmxWtiDhORRFqfNsDW48km6o",
+  authDomain: "rkfashions-cf673.firebaseapp.com",
+  projectId: "rkfashions-cf673",
+  storageBucket: "rkfashions-cf673.firebasestorage.app",
+  messagingSenderId: "285126427237",
+  appId: "1:285126427237:web:1eeceefbab182c81337c8c"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/rkfashions/icon-192.png"
+    }
+  );
+});
