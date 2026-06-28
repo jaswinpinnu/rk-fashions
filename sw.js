e5e4bd3df0bc77034dc7b9d3cb8f65ffdcb5acc1
jaswinpinnu <162045165+jaswinpinnu@users.noreply.github.com>
2026-06-28 15:18:54 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", event => {
-  console.log("Service Worker Installed");
-});
-
-self.addEventListener("activate", event => {
-  console.log("Service Worker Activated");
-});
-
-self.addEventListener("fetch", event => {});
